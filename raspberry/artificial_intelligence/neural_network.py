d34d8017860860488d01ab38c5fd76190e958a71
Romeo <romeo.correc@gmail.com>
2022-04-07 21:30:24 +0200

    Push des images + MAJ Tests + fichier réseau de neurones

--- raspberry/artificial_intelligence/neural_network.py
+++ raspberry/artificial_intelligence/neural_network.py
@@ -10,11 +10,11 @@ img_height = 180
 img_width = 180
 batch_size = 32
 
-train_ds = tf.keras.utils.image_dataset_from_directory('artificial_intelligence/resources//dechets/', validation_split=0.2, subset="training",
+train_ds = tf.keras.utils.image_dataset_from_directory('resources/dechets/', validation_split=0.2, subset="training",
                                                        seed=123, image_size=(img_height, img_width),
                                                        batch_size=batch_size)
 
-val_ds = tf.keras.utils.image_dataset_from_directory('artificial_intelligence/resources//dechets/', validation_split=0.2, subset="validation",
+val_ds = tf.keras.utils.image_dataset_from_directory('resources/dechets/', validation_split=0.2, subset="validation",
                                                      seed=123,
                                                      image_size=(img_height, img_width), batch_size=batch_size)
 
