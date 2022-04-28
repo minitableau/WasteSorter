7bdb6b4c8fa95fcff5eea3b58bc82df5b1f3a1a4
minitableau <minitableau2002@gmail.com>
2022-04-28 13:18:34 +0200

    moteur Ok & moteur et message ok

--- raspberry/servomotor/moteur.py
+++ raspberry/servomotor/moteur.py
@@ -7,12 +7,8 @@ M1_En = 21
 M1_In1 = 20
 M1_In2 = 16
 
-M2_En = 18
-M2_In1 = 23
-M2_In2 = 24
-
 # Creation d'une liste des pins pour chaque moteur pour compacter la suite du code
-Pins = [[M1_En, M1_In1, M1_In2], [M2_En, M2_In1, M2_In2]]
+Pins = [[M1_En, M1_In1, M1_In2]]
 
 # Setup
 GPIO.setmode(GPIO.BCM)
@@ -22,36 +18,20 @@ GPIO.setup(M1_En, GPIO.OUT)
 GPIO.setup(M1_In1, GPIO.OUT)
 GPIO.setup(M1_In2, GPIO.OUT)
 
-GPIO.setup(M2_En, GPIO.OUT)
-GPIO.setup(M2_In1, GPIO.OUT)
-GPIO.setup(M2_In2, GPIO.OUT)
-
-# Voir aide dans le tuto
-
-M2_Vitesse = GPIO.PWM(M2_En, 100)
-
 # Initialisation
 M1_Vitesse = GPIO.PWM(M1_En, 100)
-M1_Vitesse.start(5)  # On entre ici un rapport cyclique entre 0 et 100 (pourcentages) # inutile ?
-
-# Changement des valeurs
-M1_Vitesse.ChangeFrequency(100)  # inutile ?
-M1_Vitesse.ChangeDutyCycle(5)  # inutile ?
 
 M1_Vitesse.start(100)
-M2_Vitesse.start(100)
 
 
 def sens1(moteurNum):
     GPIO.output(Pins[moteurNum - 1][1], GPIO.HIGH)
     GPIO.output(Pins[moteurNum - 1][2], GPIO.LOW)
-    print("Moteur", moteurNum, "tourne dans le sens 1.")
 
 
 def sens2(moteurNum):
     GPIO.output(Pins[moteurNum - 1][1], GPIO.LOW)
     GPIO.output(Pins[moteurNum - 1][2], GPIO.HIGH)
-    print("Moteur", moteurNum, "tourne dans le sens 2.")
 
 
 def arret(moteurNum):
@@ -63,22 +43,23 @@ def arret(moteurNum):
 def arretComplet():
     GPIO.output(Pins[0][1], GPIO.LOW)
     GPIO.output(Pins[0][2], GPIO.LOW)
-    GPIO.output(Pins[1][1], GPIO.LOW)
-    GPIO.output(Pins[1][2], GPIO.LOW)
     print("Moteurs arretes.")
 
 
 arretComplet()
 
+print("Moteur 1 tourne dans le sens 1.")
 sens1(1)
 sleep(3)
 arretComplet()
 sleep(3)
+print("Moteur 1 tourne dans le sens 2.")
 sens2(1)
 sleep(2)
 arret(1)
 sleep(1)
 
+print("Moteur 1 tourne dans le sens 1.")
 while True:
     sens1(1)
 
