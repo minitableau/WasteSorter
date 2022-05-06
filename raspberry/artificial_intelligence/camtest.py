61cd2202c827b1aaa255f3b7cf1e5b6313fe344f
minitableau <minitableau2002@gmail.com>
2022-05-06 20:27:39 +0200

    avant fusion

--- raspberry/artificial_intelligence/camtest.py
+++ raspberry/artificial_intelligence/camtest.py
@@ -1,10 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-import time
 
 img_height = 180
 img_width = 180
